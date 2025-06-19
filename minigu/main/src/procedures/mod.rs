55b16babb0501f6d5045371165c57edb8b42514e
wzzzzd <wzd0328@outlook.com>
2025-06-19 14:50:51 +0800

    add procedure

--- minigu/main/src/procedures/mod.rs
+++ minigu/main/src/procedures/mod.rs
@@ -1,7 +1,14 @@
 mod echo;
+mod show_procedures;
 
 use minigu_context::procedure::Procedure;
 
 pub fn build_predefined_procedures() -> Vec<(String, Procedure)> {
-    vec![("echo".to_string(), echo::build_procedure())]
+    vec![
+        ("echo".to_string(), echo::build_procedure()),
+        (
+            "show_procedures".to_string(),
+            show_procedures::build_procedure(),
+        ),
+    ]
 }
