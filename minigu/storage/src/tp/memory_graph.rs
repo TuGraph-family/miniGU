4d274890aeb8f26701af8114fdfdab6978e1da3f
Sober7135 <soberwu@outlook.com>
2025-07-07 21:02:50 +0800

    support import and export

--- minigu/storage/src/tp/memory_graph.rs
+++ minigu/storage/src/tp/memory_graph.rs
@@ -966,8 +966,8 @@ pub mod tests {
     use crate::model::properties::PropertyRecord;
 
     const PERSON: LabelId = LabelId::new(1).unwrap();
-    const FRIEND: LabelId = LabelId::new(1).unwrap();
-    const FOLLOW: LabelId = LabelId::new(2).unwrap();
+    const FRIEND: LabelId = LabelId::new(2).unwrap();
+    const FOLLOW: LabelId = LabelId::new(3).unwrap();
 
     fn create_vertex(id: VertexId, label_id: LabelId, properties: Vec<ScalarValue>) -> Vertex {
         Vertex::new(id, label_id, PropertyRecord::new(properties))
@@ -1809,5 +1809,410 @@ pub mod tests {
             recovered_vertex2.properties()[0],
             ScalarValue::String(Some("After Checkpoint".to_string()))
         );
+    }
+
+    mod import_and_export {
+        use std::collections::HashMap;
+        use std::path::Path;
+
+        use csv::{ReaderBuilder, WriterBuilder};
+        use minigu_catalog::label_set::LabelSet;
+        use minigu_catalog::memory::graph_type::{
+            MemoryEdgeTypeCatalog, MemoryGraphTypeCatalog, MemoryVertexTypeCatalog,
+        };
+        use minigu_catalog::property::Property;
+        use minigu_catalog::provider::GraphTypeProvider;
+        use minigu_common::data_type::*;
+        use serde::{Deserialize, Serialize};
+
+        use super::*;
+
+        #[derive(Deserialize, Serialize)]
+        struct FileConfig {
+            path: String,
+            // Current only support CSV, consider using enum
+            format: String,
+        }
+
+        impl FileConfig {
+            pub fn new(path: String, format: String) -> Self {
+                Self { path, format }
+            }
+        }
+
+        #[derive(Deserialize, Serialize)]
+        struct VertexConfig {
+            label: String,
+            file: FileConfig,
+        }
+
+        impl VertexConfig {
+            pub fn new(label: String, file: FileConfig) -> Self {
+                Self { label, file }
+            }
+        }
+
+        #[derive(Deserialize, Serialize)]
+        struct EdgeConfig {
+            label: String,
+            file: FileConfig,
+        }
+
+        impl EdgeConfig {
+            pub fn new(label: String, file: FileConfig) -> Self {
+                Self { label, file }
+            }
+        }
+
+        #[derive(Deserialize, Serialize, Default)]
+        struct Config {
+            vertex_config_list: Vec<VertexConfig>,
+            edge_config_list: Vec<EdgeConfig>,
+        }
+
+        impl Config {
+            pub fn new(
+                vertex_config_list: Vec<VertexConfig>,
+                edge_config_list: Vec<EdgeConfig>,
+            ) -> Self {
+                Self {
+                    vertex_config_list,
+                    edge_config_list,
+                }
+            }
+
+            pub fn add_vertex_config(&mut self, config: VertexConfig) {
+                self.vertex_config_list.push(config);
+            }
+
+            pub fn add_edge_config(&mut self, config: EdgeConfig) {
+                self.edge_config_list.push(config);
+            }
+        }
+
+        // label_id -> label_set
+        fn find_label_set_by_label_id(
+            label_id: LabelId,
+            label_set_list: &Vec<LabelSet>,
+        ) -> Option<&LabelSet> {
+            label_set_list.iter().find(|&ls| ls.contains(label_id))
+        }
+
+        fn property_to_scalar_value(property: &Property, value: &str) -> ScalarValue {
+            match property.logical_type() {
+                LogicalType::Int8 => ScalarValue::Int8(Some(value.parse().unwrap())),
+                LogicalType::Int16 => ScalarValue::Int16(Some(value.parse().unwrap())),
+                LogicalType::Int32 => ScalarValue::Int32(Some(value.parse().unwrap())),
+                LogicalType::Int64 => ScalarValue::Int64(Some(value.parse().unwrap())),
+                LogicalType::UInt8 => ScalarValue::UInt8(Some(value.parse().unwrap())),
+                LogicalType::UInt16 => ScalarValue::UInt16(Some(value.parse().unwrap())),
+                LogicalType::UInt32 => ScalarValue::UInt32(Some(value.parse().unwrap())),
+                LogicalType::UInt64 => ScalarValue::UInt64(Some(value.parse().unwrap())),
+                LogicalType::Boolean => ScalarValue::Boolean(Some(value.parse().unwrap())),
+                LogicalType::Float32 => ScalarValue::Float32(Some(value.parse().unwrap())),
+                LogicalType::Float64 => ScalarValue::Float64(Some(value.parse().unwrap())),
+                LogicalType::String => ScalarValue::String(Some(value.to_string())),
+                _ => todo!(),
+            }
+        }
+
+        // fn find_name_by_label_id(
+        //     schema: &MemoryGraphTypeCatalog,
+        //     label_id: LabelId,
+        // ) -> Option<String> {
+        //     // schema
+        //     //     .label_map
+        //     //     .iter()
+        //     //     .find(|&(_, &v)| v == label_id)
+        //     //     .map(|(k, _)| k)
+        //     None
+        // }
+
+        fn scalar_value_to_string(scalar_value: &ScalarValue) -> String {
+            match scalar_value {
+                ScalarValue::Int8(x) => x.unwrap().to_string(),
+                ScalarValue::Int16(x) => x.unwrap().to_string(),
+                ScalarValue::Int32(x) => x.unwrap().to_string(),
+                ScalarValue::Int64(x) => x.unwrap().to_string(),
+                ScalarValue::UInt8(x) => x.unwrap().to_string(),
+                ScalarValue::UInt16(x) => x.unwrap().to_string(),
+                ScalarValue::UInt32(x) => x.unwrap().to_string(),
+                ScalarValue::UInt64(x) => x.unwrap().to_string(),
+                ScalarValue::Boolean(x) => x.unwrap().to_string(),
+                ScalarValue::Float32(x) => x.unwrap().to_string(),
+                ScalarValue::Float64(x) => x.unwrap().to_string(),
+                ScalarValue::String(x) => x.as_ref().unwrap().clone(),
+                _ => todo!(),
+            }
+        }
+
+        impl MemoryGraph {
+            // TODO: return `Result`
+            pub fn import<P: AsRef<Path>>(
+                &self,
+                txn: &TransactionHandle,
+                config_path: P,
+                schema: &MemoryGraphTypeCatalog,
+            ) {
+                let config_str = fs::read_to_string(config_path.as_ref()).unwrap();
+                let config: Config = serde_json::from_str(&config_str).unwrap();
+
+                // Import vertex, format: "<vid>,<property-1>,<property-2>,"
+                for vertex_config in config.vertex_config_list.iter() {
+                    let mut rdr = ReaderBuilder::new()
+                        .has_headers(false)
+                        .from_path(
+                            config_path
+                                .as_ref()
+                                .parent()
+                                .unwrap()
+                                .join(&vertex_config.file.path),
+                        )
+                        .unwrap();
+
+                    let label_id = schema.get_label_id(&vertex_config.label).unwrap().unwrap();
+
+                    for record in rdr.records() {
+                        let label_set_list = schema.vertex_type_keys();
+                        let label_set =
+                            find_label_set_by_label_id(label_id, &label_set_list).unwrap();
+                        let props = schema
+                            .get_vertex_type(label_set)
+                            .unwrap()
+                            .unwrap()
+                            .properties();
+
+                        let record = record.unwrap();
+
+                        assert_eq!(props.len() + 1, record.len());
+
+                        let vid = record.get(0).unwrap().parse().unwrap();
+
+                        let props = props
+                            .iter()
+                            .zip(record.iter().skip(1))
+                            .map(|((_, p), value)| property_to_scalar_value(p, value))
+                            .collect();
+                        let vertex = Vertex::new(vid, label_id, PropertyRecord::new(props));
+
+                        self.create_vertex(txn, vertex).unwrap();
+                    }
+                }
+
+                // Import edge, format: "<eid>,<src>,<dst>,<property-1>,<property-2>"
+                for edge_config in config.edge_config_list.iter() {
+                    let label_id = schema.get_label_id(&edge_config.label).unwrap().unwrap();
+
+                    let mut rdr = ReaderBuilder::new()
+                        .has_headers(false)
+                        .from_path(
+                            config_path
+                                .as_ref()
+                                .parent()
+                                .unwrap()
+                                .join(&edge_config.file.path),
+                        )
+                        .unwrap();
+
+                    for record in rdr.records() {
+                        let label_set_list = schema.edge_type_keys();
+                        let label_set =
+                            find_label_set_by_label_id(label_id, &label_set_list).unwrap();
+
+                        let props = schema
+                            .get_edge_type(label_set)
+                            .unwrap()
+                            .unwrap()
+                            .properties();
+
+                        let record = record.unwrap();
+
+                        assert_eq!(record.len() - 3, props.len());
+                        let eid = record.get(0).unwrap().parse().unwrap();
+                        let src_id = record.get(1).unwrap().parse().unwrap();
+                        let dst_id = record.get(2).unwrap().parse().unwrap();
+
+                        let props = props
+                            .iter()
+                            .zip(record.iter().skip(3))
+                            .map(|((_, p), value)| property_to_scalar_value(p, value))
+                            .collect();
+
+                        let edge =
+                            Edge::new(eid, src_id, dst_id, label_id, PropertyRecord::new(props));
+                        self.create_edge(txn, edge).unwrap();
+                    }
+                }
+            }
+
+            pub fn export<P: AsRef<Path>>(
+                &self,
+                txn: &TransactionHandle,
+                dir: P,
+                schema: &MemoryGraphTypeCatalog,
+                config_file_name: String,
+            ) {
+                // 1. prepare output paths
+                let dir = dir.as_ref();
+                fs::create_dir_all(dir).unwrap();
+
+                let (label_id_to_label, mut file_map): (HashMap<_, _>, HashMap<_, _>) = schema
+                    .label_names()
+                    .iter()
+                    .map(|label| {
+                        let filename = format!("{label}.csv");
+                        let path = dir.join(&filename);
+                        let label_id = schema.get_label_id(label).unwrap().unwrap();
+
+                        (
+                            (label_id, label.to_string()),
+                            (label_id, WriterBuilder::new().from_path(path).unwrap()),
+                        )
+                    })
+                    .unzip();
+
+                let mut vertex_config_map = HashMap::new();
+                // 2. export vertices
+                for v in txn.iter_vertices() {
+                    let v = v.unwrap();
+                    let w = file_map.get_mut(&v.label_id).unwrap();
+
+                    // Insert vertex config
+                    vertex_config_map.entry(v.label_id).or_insert_with(|| {
+                        let label = label_id_to_label.get(&v.label_id).unwrap();
+                        VertexConfig::new(
+                            label.clone(),
+                            FileConfig::new(format!("{}.csv", label), "csv".to_string()),
+                        )
+                    });
+
+                    let record = std::iter::once(v.vid().to_string())
+                        .chain(v.properties().iter().map(scalar_value_to_string))
+                        .collect::<Vec<_>>();
+
+                    w.write_record(record).unwrap();
+                }
+                let vertex_config_list = vertex_config_map
+                    .into_iter()
+                    .map(|(_, config)| config)
+                    .collect();
+
+                // 3. export edge
+                let mut edge_config_map = HashMap::new();
+                for e in txn.iter_edges() {
+                    let e = e.unwrap();
+                    let w = file_map.get_mut(&e.label_id).unwrap();
+
+                    // Insert edge config
+                    edge_config_map.entry(e.label_id).or_insert_with(|| {
+                        let label = label_id_to_label.get(&e.label_id).unwrap();
+                        EdgeConfig::new(
+                            label.clone(),
+                            FileConfig::new(format!("{}.csv", label), "csv".to_string()),
+                        )
+                    });
+
+                    let prefix = [
+                        e.eid().to_string(),
+                        e.src_id().to_string(),
+                        e.dst_id().to_string(),
+                    ];
+                    let record = prefix
+                        .into_iter()
+                        .chain(e.properties().iter().map(scalar_value_to_string))
+                        .collect::<Vec<_>>();
+
+                    println!("{:?}", record);
+                    w.write_record(record).unwrap();
+                }
+                let edge_config_list = edge_config_map
+                    .into_iter()
+                    .map(|(_, config)| config)
+                    .collect();
+
+                // 4. write config file
+                let config = Config::new(vertex_config_list, edge_config_list);
+                fs::write(
+                    dir.join(config_file_name),
+                    serde_json::to_string(&config).unwrap(),
+                )
+                .unwrap();
+            }
+        }
+
+        fn mock_schema() -> MemoryGraphTypeCatalog {
+            let mut schema = MemoryGraphTypeCatalog::new();
+            let person_id = schema.add_label("person".to_string()).unwrap();
+            let friend_id = schema.add_label("friend".to_string()).unwrap();
+            let follow_id = schema.add_label("follow".to_string()).unwrap();
+
+            let person_label_set = LabelSet::from_iter([person_id]);
+            let friend_label_set = LabelSet::from_iter([friend_id]);
+            let follow_label_set = LabelSet::from_iter([follow_id]);
+
+            let vertex_type = Arc::new(MemoryVertexTypeCatalog::new(
+                person_label_set.clone(),
+                vec![
+                    Property::new("name".to_string(), LogicalType::String, false),
+                    Property::new("age".to_string(), LogicalType::Int32, false),
+                ],
+            ));
+
+            schema.add_vertex_type(person_label_set, vertex_type.clone());
+            schema.add_edge_type(
+                friend_label_set.clone(),
+                Arc::new(MemoryEdgeTypeCatalog::new(
+                    friend_label_set,
+                    vertex_type.clone(),
+                    vertex_type.clone(),
+                    vec![Property::new(
+                        "date".to_string(),
+                        LogicalType::String,
+                        false,
+                    )],
+                )),
+            );
+            schema.add_edge_type(
+                follow_label_set.clone(),
+                Arc::new(MemoryEdgeTypeCatalog::new(
+                    follow_label_set,
+                    vertex_type.clone(),
+                    vertex_type.clone(),
+                    vec![Property::new(
+                        "date".to_string(),
+                        LogicalType::String,
+                        false,
+                    )],
+                )),
+            );
+
+            schema
+        }
+
+        #[test]
+        fn import_and_export() {
+            let schema = mock_schema();
+            {
+                let (graph, _cleaner) = mock_graph();
+                let txn = graph.begin_transaction(IsolationLevel::Serializable);
+
+                println!("==================== export start ====================");
+                graph.export(&txn, "/tmp/minigu", &schema, "config.json".to_string());
+                println!("==================== export done ====================");
+            }
+
+            {
+                let (graph, _) = mock_empty_graph();
+                let txn = graph.begin_transaction(IsolationLevel::Serializable);
+
+                println!("==================== import start ====================");
+                graph.import(&txn, "/tmp/minigu/config.json", &schema);
+                println!("==================== import done ====================");
+
+                println!("==================== export start ====================");
+                graph.export(&txn, "/tmp/minigu-test", &schema, "config.json".to_string());
+                println!("==================== export done ====================");
+            }
+        }
     }
 }
