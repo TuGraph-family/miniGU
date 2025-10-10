866c3167e119687dac1018169358da14fabeb421
Boat2U <1589766453@qq.com>
2025-10-10 22:21:55 +0800

    Squashed 'minigu/storage/DiskANN-rs/' content from commit 4cf902f
    git-subtree-dir: minigu/storage/DiskANN-rs
    git-subtree-split: 4cf902f3aa451c81a72f5a8d81c2ac53aa1a6a4a

---
+++ DiskANN/src/index/mod.rs
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+mod inmem_index;
+pub use inmem_index::InmemIndex;
+pub use inmem_index::ann_inmem_index::*;
+
+mod disk_index;
+pub use disk_index::*;
