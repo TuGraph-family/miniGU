edde07dbaf112c10464f9ebb84bf765cf077ebf2
Boat2U <1589766453@qq.com>
2025-10-10 22:17:18 +0800

    fix(None):rm diskann-rs

--- diskann-rs/DiskANN/src/model/aligned_file_reader/mod.rs
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-#[allow(clippy::module_inception)]
-mod aligned_file_reader;
-pub use aligned_file_reader::*;
