1cdcf97e997e4f145ac8826d90c67aeeb2f6fb64
Boat2U <1589766453@qq.com>
2025-10-16 03:26:45 +0800

    feature(storage): import diskann-rs

---
+++ minigu/storage/diskann-rs/vector/src/lib.rs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+#![cfg(all(target_os = "linux", feature = "vector-support"))]
+#![cfg_attr(
+    not(test),
+    warn(clippy::panic, clippy::unwrap_used, clippy::expect_used)
+)]
+
+// #![feature(stdsimd)]
+// mod f32x16;
+// Uncomment above 2 to experiment with f32x16
+mod distance;
+mod l2_float_distance;
+mod metric;
+mod utils;
+
+pub use distance::FullPrecisionDistance;
+pub use l2_float_distance::distance_l2_vector_f32;
+pub use metric::Metric;
+pub use utils::prefetch_vector;
+
+#[cfg(all(test, target_os = "linux", feature = "vector-support"))]
+mod distance_test;
+mod test_util;
